f7d831d3bd0247ef4a7d9cd875821c664cec3f0a
whlte11 <kvlad3876@gmail.com>
2023-05-13 01:31:12 +0300

    1

--- js/index.js
+++ js/index.js
@@ -3,7 +3,7 @@ const options = document.querySelectorAll(".option input");
 const copyIcon = document.querySelector(".input-box span");
 const passwordInput = document.querySelector(".input-box input");
 const passIndicator = document.querySelector(".pass-indicator");
-const generateBtn = document.querySelector(".gnr-btn");
+const generateBtn = document.querySelector(".generate-btn");
 
 const characters = {
     lowercase: "abcdefghijklmnopqrstuvwxyz",
